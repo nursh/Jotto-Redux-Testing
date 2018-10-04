87556a5ee0e86fd1bfc0d866b1351f6f18c48968
nursh <nsheikhmohammed@yahoo.com>
2018-10-04 08:38:37 -0400

    Added GuessedWords component with accompanying test file

---
+++ jotto/src/GuessedWords.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+
+import { findByTestAttr, checkProps } from '../tests/testutils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [
+    { guessedWord: 'train', letterMatchCount: 3},
+  ],
+}
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+}
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+})
+
+describe('if there are no words guessed', () => {
+
+})
+
+describe('if there are words guessed', () => {
+  
+})
