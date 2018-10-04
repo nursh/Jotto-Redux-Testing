911ba602c75ac8b41d66c7bb3bf1835326586394
nursh <nsheikhmohammed@yahoo.com>
2018-10-04 07:01:39 -0400

    Wrote test to check if Congrats Component renders without error

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -12,7 +12,9 @@ const setup = (props = {}) => {
 };
 
 test('renders without error', () => {
-
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
 });
 
 test('renders no text when `success` prop is false', () => {
