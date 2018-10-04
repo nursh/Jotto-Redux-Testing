f7a5bc8218cbb4e4df6c661a5070e6eab83682c7
nursh <nsheikhmohammed@yahoo.com>
2018-10-04 07:02:51 -0400

    Implemented test for no text when success props is false

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -18,7 +18,9 @@ test('renders without error', () => {
 });
 
 test('renders no text when `success` prop is false', () => {
-
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe('');
 });
 
 test('renders non-empty congrats message when `success` props is true', () => {
